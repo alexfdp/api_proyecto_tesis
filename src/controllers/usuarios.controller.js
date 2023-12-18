72242d8d074386534bbabe7937d1439944f1f391
Alex Fajardo <alexf.124@hotmail.com>
2023-12-18 15:11:15 -0500

    commit 19

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -4,7 +4,7 @@ export const consultarAllUsers = async (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
     try {
         const { id } = req.params;
-        const [result] = await pool.query(`SELECT u.iduser, e.idempleado, 
+        const [result] = await pool.query(`SELECT u.iduser, u.rol_id, e.idempleado, 
             CONCAT_WS(' ', e.nombre, e.apellido, e.apellido_2) nombre, 
             u.usuario, u.estado, u.fecha_registro 
             FROM usuario AS u 
@@ -21,6 +21,33 @@ export const consultarAllUsers = async (req, res) => {
         console.log("error: " + error.message)
         res.status(500).json({
             message: "Ha ocurrido un error: " + error.message
+        })
+    }
+}
+
+export const updateUsuario = async (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    try {
+        const { iduser, usuario, contrasena } = req.body;
+        const [result] = await pool.query(`UPDATE usuario AS u 
+            SET u.usuario = ?, u.contrasena = ? 
+            WHERE u.iduser = ? AND u.estado != 0;`,
+            [usuario, contrasena, iduser]);
+        if (result.affectedRows <= 0) {
+            console.log('No se pudo actualizar usuario')
+            res.status(400).json({
+                message: 'No se pudo actualizar usuario'
+            })
+        } else {
+            console.log('Usuario actualizado con éxito')
+            res.status(200).json({
+                message: 'Usuario actualizado con éxito'
+            })
+        }
+    } catch (error) {
+        console.log("error update user: " + error.message);
+        res.status(500).json({
+            message: "Ha ocurrido un error al actualizar usuario: " + error.message
         })
     }
 }
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -4,5 +4,6 @@ import { verifytoken } from '../middlewares/authJWT.js';
 const router = Router()
 
 router.get('/usuario', verifytoken, us.consultarAllUsers);
+router.put('/usuario', verifytoken, us.updateUsuario);
 
 export default router;
