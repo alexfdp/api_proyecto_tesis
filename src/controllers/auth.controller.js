c36f4208ce5526fbc03c494e4b21c5334f4c3883
Alex Fajardo <alexf.124@hotmail.com>
2023-12-22 16:04:06 -0500

    commit 22

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,8 @@
 import jwt from 'jsonwebtoken';
 import { pool } from '../db.js';
-import { User } from '../models/User.js'
-import * as cifr from '../models/cifrar.js'
-import config from '../config.js'
+import { User } from '../models/User.js';
+import * as cifr from '../models/cifrar.js';
+import config from '../config.js';
 
 export const autenticar = async (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -38,6 +38,34 @@ export const autenticar = async (req, res) => {
         console.log("error: " + error.message)
         res.status(500).json({
             message: "Ha ocurrido un error: " + error.message
+        })
+    }
+}
+
+export const changePass = async (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    try {
+        const { id } = req.params;
+        const { contrasena } = req.body;
+        const cr = await cifr.cifrar(contrasena);
+        console.log("contrasena env: " + contrasena + "\nid: " + id + "\ncontrasena cifrada: " + cr)
+        const [result] = await pool.query('UPDATE usuario SET contrasena = ?, estado = 1 WHERE iduser = ? AND ESTADO = 2'
+            , [cr, id]);
+        if (result.affectedRows <= 0) {
+            console.log('No se pudo actualizar contraseña')
+            res.status(400).json({
+                message: 'No se pudo actualizar contraseña'
+            })
+        } else {
+            console.log('Contraseña actualizada con éxito')
+            res.status(200).json({
+                message: 'Contraseña actualizada con éxito'
+            })
+        }
+    } catch (error) {
+        console.log("Error update password: " + error.message);
+        res.status(500).json({
+            message: "Ha ocurrido un error al actualizar contraseña: " + error.message
         })
     }
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import * as authCtrl from "../controllers/auth.controller.js";
+import { verifytoken } from '../middlewares/authJWT.js';
 const router = Router()
 
 router.post('/', authCtrl.autenticar);
+router.put('/changePass', verifytoken, authCtrl.changePass)
 
 export default router;
