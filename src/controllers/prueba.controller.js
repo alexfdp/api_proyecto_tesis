e5280ffb5447c8782e202da3899700758a42e396
Alex Fajardo <alexf.124@hotmail.com>
2024-04-23 08:49:24 -0500

    cambios en endpoint de prueba SSFF

--- src/controllers/prueba.controller.js
+++ src/controllers/prueba.controller.js
@@ -9,10 +9,10 @@ export const validarOPR = async (req, res) => {
         // if (codigoSap == 10243) {
         //     resultado = '{"Codigo": "1","Mensaje Error": "Empleado No Existe"}';
         // }
-        if (codigoSap == 55886) {
+        if (codigoSap === "55886") {
             resultado = '{"Codigo": "1","Mensaje Error": "Empleado Cesado"}';
         }
-        if (codigoSap == 105692) {
+        if (codigoSap === "105692") {
             resultado = '{"Codigo": "0 Proceso Exitoso","CodCargo": "450","Cargo": "SUPERVISOR DE SEGURIDAD","CodTienda": "W001","Tienda": "Guayaquil_AdePaSA-C.C.CeibosADPS"}';
         }
         //console.log(resultado);
